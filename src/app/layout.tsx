e1c36179a834f61d7e1720c91256a9189a4ab995
Meow-peri <priyanka.mishra.id26@gmail.com>
2026-08-25 16:21:13 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,13 @@ export const metadata: Metadata = {
     "women's health",
     "protocol",
   ],
+    manifest: "/manifest.json",
+    icons: {
+          icon: [
+            { url: "/icon-192.png", sizes: "192x192", type: "image/png" },
+            { url: "/icon-512.png", sizes: "512x512", type: "image/png" },
+                ],
+    },
 };
 
 export const viewport: Viewport = {
